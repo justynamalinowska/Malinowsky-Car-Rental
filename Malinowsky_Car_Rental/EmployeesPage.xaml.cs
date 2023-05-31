cd248cbe6e8c8a2c7b63b3d45c51ce3b23a12538
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-05-31 20:23:15 +0200

    Create `Employees` Page

---
+++ Malinowsky_Car_Rental/EmployeesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Malinowsky_Car_Rental
+{
+    /// <summary>
+    /// Interaction logic for EmployeesPage.xaml
+    /// </summary>
+    public partial class EmployeesPage : Window
+    {
+        public EmployeesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
