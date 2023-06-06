56c291f6353c168762a8b0e00abe358b946d5bef
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-06-06 21:02:14 +0200

    Update `Grid`

--- Malinowsky_Car_Rental/Views/CustomersList.xaml.cs
+++ Malinowsky_Car_Rental/Views/CustomersList.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Malinowsky_Car_Rental.DB;
 
 namespace Malinowsky_Car_Rental.Views
 {
