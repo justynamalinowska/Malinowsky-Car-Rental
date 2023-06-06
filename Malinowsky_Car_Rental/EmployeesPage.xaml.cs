321034e2b44517597f0f610c9d0d41a502341b99
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-06-06 19:48:46 +0200

    Fix `btnSave_Click` for updating informations

--- Malinowsky_Car_Rental/EmployeesPage.xaml.cs
+++ Malinowsky_Car_Rental/EmployeesPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Malinowsky_Car_Rental.DB;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,46 +26,79 @@ namespace Malinowsky_Car_Rental
             Close();
         }
 
+        public Pracownicy pracownicy;
+
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            if (cmbBaseId.SelectedItem != null && txtPESEL.Text.Length == 11 && short.Parse(txtHourlyRate.Text) > 0)
+            if (cmbBaseId.SelectedItem != null && txtPESEL.Text.Length == 11)
             {
                 int idBazy = ((ComboValue)cmbBaseId.SelectedItem).Id;
 
                 using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
                 {
-                    Pracownicy emp = new Pracownicy();
-                    emp.IdBazy = idBazy;
-                    emp.Pesel = txtPESEL.Text;
-                    emp.Imie = txtName.Text;
-                    emp.Nazwisko = txtSurname.Text;
-                    emp.Stanowisko = txtPosition.Text;
-                    emp.StawkaGodzinowa = short.Parse(txtHourlyRate.Text);
-                    emp.Kraj = txtCountry.Text;
-                    emp.Miasto = txtCity.Text;
-                    emp.Ulica = txtStreet.Text;
-                    emp.NumerLokalu = txtApartmentNo.Text;
-                    emp.NumerDomu = txtHouseNo.Text;
-                    emp.NrTelefonu = txtContactNo.Text;
+                    if (pracownicy != null && pracownicy.IdPracownika != 0)
+                    {
+                        int idPracownika = pracownicy.IdPracownika;
+                        Pracownicy update = db.Pracownicy.FirstOrDefault(p => p.IdPracownika == idPracownika);
+                        if (update == null)
+                        {
+                            MessageBox.Show("Employee not found!");
+                        }
+                        else
+                        {
+                            update.IdBazy = idBazy;
+                            update.Pesel = txtPESEL.Text;
+                            update.Imie = txtName.Text;
+                            update.Nazwisko = txtSurname.Text;
+                            update.Stanowisko = txtPosition.Text;
+                            update.StawkaGodzinowa = short.Parse(txtHourlyRate.Text);
+                            update.Kraj = txtCountry.Text;
+                            update.Miasto = txtCity.Text;
+                            update.Ulica = txtStreet.Text;
+                            update.NumerLokalu = txtApartmentNo.Text;
+                            update.NumerDomu = txtHouseNo.Text;
+                            update.NrTelefonu = txtContactNo.Text;
 
-                    db.Pracownicy.Add(emp);
-                    db.SaveChanges();
+                            db.SaveChanges();
+
+                            MessageBox.Show("Informations have been updated!");
+                        }
+                    }
+
+                    else
+                    {
+                        Pracownicy emp = new Pracownicy();
+                        emp.IdBazy = cmbBaseId.SelectedIndex;
+                        emp.Pesel = txtPESEL.Text;
+                        emp.Imie = txtName.Text;
+                        emp.Nazwisko = txtSurname.Text;
+                        emp.Stanowisko = txtPosition.Text;
+                        emp.StawkaGodzinowa = short.Parse(txtHourlyRate.Text);
+                        emp.Kraj = txtCountry.Text;
+                        emp.Miasto = txtCity.Text;
+                        emp.Ulica = txtStreet.Text;
+                        emp.NumerLokalu = txtApartmentNo.Text;
+                        emp.NumerDomu = txtHouseNo.Text;
+                        emp.NrTelefonu = txtContactNo.Text;
+
+                        db.Pracownicy.Add(emp);
+                        db.SaveChanges();
+
+                        MessageBox.Show("Employee has been added!");
+                    }
+
+                    txtName.Clear();
+                    txtSurname.Clear();
+                    txtPESEL.Clear();
+                    txtPosition.Clear();
+                    txtHourlyRate.Clear();
+                    txtCountry.Clear();
+                    txtCity.Clear();
+                    txtStreet.Clear();
+                    txtApartmentNo.Clear();
+                    txtHouseNo.Clear();
+                    txtContactNo.Clear();
                 }
-
-                txtName.Clear();
-                txtSurname.Clear();
-                txtPESEL.Clear();
-                txtPosition.Clear();
-                txtHourlyRate.Clear();
-                txtCountry.Clear();
-                txtCity.Clear();
-                txtStreet.Clear();
-                txtApartmentNo.Clear();
-                txtHouseNo.Clear();
-                txtContactNo.Clear();
-
-                MessageBox.Show("Employee has been added");
-
             }
             else
             {
@@ -87,6 +121,25 @@ namespace Malinowsky_Car_Rental
             cmbBaseId.ItemsSource = list;
             cmbBaseId.DisplayMemberPath = "Name";
             cmbBaseId.SelectedValuePath = "Id";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (pracownicy != null && pracownicy.IdPracownika != 0)
+            {
+                txtName.Text = pracownicy.Imie;
+                txtSurname.Text = pracownicy.Nazwisko;
+                txtPESEL.Text = pracownicy.Pesel;
+                txtPosition.Text = pracownicy.Stanowisko;
+                txtHourlyRate.Text = pracownicy.StawkaGodzinowa.ToString();
+                txtCountry.Text = pracownicy.Kraj;
+                txtCity.Text = pracownicy.Miasto;
+                txtStreet.Text = pracownicy.Ulica;
+                txtApartmentNo.Text = pracownicy.NumerLokalu;
+                txtHouseNo.Text = pracownicy.NumerDomu;
+                txtContactNo.Text = pracownicy.NrTelefonu;
+                cmbBaseId.Text = pracownicy.IdBazy.ToString(); 
+            }
         }
     }
 
