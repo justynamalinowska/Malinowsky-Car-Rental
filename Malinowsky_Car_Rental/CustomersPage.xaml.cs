b742a2c7b6652e0e7dce120a33c304b6ef882bfd
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-06-07 09:27:05 +0200

    Create `btnAdd_Click` and `btnUpdate_Click`

--- Malinowsky_Car_Rental/CustomersPage.xaml.cs
+++ Malinowsky_Car_Rental/CustomersPage.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Linq;
+using Malinowsky_Car_Rental.DB;
 
 namespace Malinowsky_Car_Rental
 {
@@ -22,9 +24,24 @@ namespace Malinowsky_Car_Rental
             InitializeComponent();
         }
 
+        public Klienci klienci;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            if (klienci != null && pracownicy.IdPracownika != 0)
+            {
+                txtName.Text = pracownicy.Imie;
+                txtSurname.Text = pracownicy.Nazwisko;
+                txtPESEL.Text = pracownicy.Pesel;
+                txtPosition.Text = pracownicy.Stanowisko;
+                txtHourlyRate.Text = pracownicy.StawkaGodzinowa.ToString();
+                txtCountry.Text = pracownicy.Kraj;
+                txtCity.Text = pracownicy.Miasto;
+                txtStreet.Text = pracownicy.Ulica;
+                txtApartmentNo.Text = pracownicy.NumerLokalu;
+                txtHouseNo.Text = pracownicy.NumerDomu;
+                txtContactNo.Text = pracownicy.NrTelefonu;
+                cmbBaseId.Text = pracownicy.IdBazy.ToString();
+            }
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
@@ -34,7 +51,7 @@ namespace Malinowsky_Car_Rental
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
     }
 }
