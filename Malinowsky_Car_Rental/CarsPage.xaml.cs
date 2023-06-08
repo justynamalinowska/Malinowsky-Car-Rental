2d654fc070aa573ad028b8e9a65ac8f38121749b
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-06-08 12:57:01 +0200

    Add `btnSave_Click`, `btnClose_Click` and `Window_Loaded`

--- Malinowsky_Car_Rental/CarsPage.xaml.cs
+++ Malinowsky_Car_Rental/CarsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Malinowsky_Car_Rental.DB;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +22,103 @@ namespace Malinowsky_Car_Rental
         public CarsPage()
         {
             InitializeComponent();
+        }
+        public Samochody samochody;
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbBaseId.SelectedItem != null && cmbTypeId.SelectedItem != null && !string.IsNullOrEmpty(txtVin.Text) && txtVin.Text.Length == 17 )
+            {
+                int idBazy = (int)cmbBaseId.SelectedValue;
+                int idTypu = (int)cmbTypeId.SelectedValue;
+
+                using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+                {
+                    if (samochody != null && samochody.IdSamochodu != 0)
+                    {
+                        int idSamochodu = samochody.IdSamochodu;
+                        Samochody update = db.Samochody.FirstOrDefault(s => s.IdSamochodu == idSamochodu);
+                        if (update == null)
+                        {
+                            MessageBox.Show("Car not found!");
+                        }
+                        else
+                        {
+                            update.IdBazy = idBazy;
+                            update.IdTypu = idTypu;
+                            update.Vin = txtVin.Text;
+                            update.Przebieg = int.Parse(txtMileage.Text);
+                            update.CenaZaDzien = short.Parse(txtPricePerDay.Text);
+
+                            db.SaveChanges();
+
+                            MessageBox.Show("Information has been updated!");
+                        }
+                    }
+                    else
+                    {
+                        Samochody samochody = new Samochody();
+                        samochody.IdBazy = idBazy;
+                        samochody.IdTypu = idTypu;
+                        samochody.Vin = txtVin.Text;
+                        samochody.Przebieg = int.Parse(txtMileage.Text);
+                        samochody.CenaZaDzien = short.Parse(txtPricePerDay.Text);
+
+                        db.Samochody.Add(samochody);
+                        db.SaveChanges();
+
+                        MessageBox.Show("Car has been added!");
+                    }
+
+                    cmbTypeId.SelectedItem = null;
+                    cmbBaseId.SelectedItem = null;
+                    txtVin.Clear();
+                    txtMileage.Clear();
+                    txtPricePerDay.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select base ID, type ID and enter a valid VIN.");
+            }
+        }
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+                
+                List<Bazy> bazy = db.Bazy.ToList();
+
+                
+                cmbBaseId.ItemsSource = bazy;
+                cmbBaseId.DisplayMemberPath = "IdBazy"; 
+                cmbBaseId.SelectedValuePath = "IdBazy";
+
+                
+                List<TypySamochodow> typySamochodow = db.TypySamochodow.ToList();
+
+                
+                cmbTypeId.ItemsSource = typySamochodow;
+                cmbTypeId.DisplayMemberPath = "IdTypu"; 
+                cmbTypeId.SelectedValuePath = "IdTypu";
+            }
+
+            if (samochody != null && samochody.IdSamochodu != 0)
+            {
+                txtVin.Text = samochody.Vin;
+                txtMileage.Text = samochody.Przebieg.ToString();
+                txtPricePerDay.Text = samochody.CenaZaDzien.ToString();
+                cmbBaseId.SelectedValue = samochody.IdBazy;
+                cmbTypeId.SelectedValue = samochody.IdTypu;
+            }
+
+
         }
     }
 }
