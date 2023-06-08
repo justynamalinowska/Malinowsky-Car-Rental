679a28b62ddccc51f01119d89358263c3dd3b688
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-06-08 11:03:04 +0200

    Add `btnAdd_Click`, `btnUpdate_Click` and `btnDelete_Click`

--- Malinowsky_Car_Rental/Views/CarsList.xaml.cs
+++ Malinowsky_Car_Rental/Views/CarsList.xaml.cs
@@ -29,5 +29,50 @@ namespace Malinowsky_Car_Rental.Views
                 gridCars.ItemsSource = list;
             }
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            CarsPage page = new CarsPage();
+            page.ShowDialog();
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+                List<Samochody> list = db.Samochody.ToList();
+                gridCars.ItemsSource = list;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Samochody sam = (Samochody)gridCars.SelectedItem;
+            CarsPage page = new CarsPage();
+            page.samochody = sam;
+            page.ShowDialog();
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+                gridCars.ItemsSource = db.Samochody.OrderBy(x => x.IdSamochodu).ToList();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this record?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Samochody sam = (Samochody)gridCars.SelectedItem;
+                if (sam != null)
+                {
+                    using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+                    {
+                        db.Samochody.Remove(sam);
+                        db.SaveChanges();
+                        MessageBox.Show("Record deleted successfully!");
+                        gridCars.ItemsSource = db.Samochody.OrderBy(x => x.IdSamochodu).ToList();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No record selected!");
+                }
+            }
+        }
     }
 }
