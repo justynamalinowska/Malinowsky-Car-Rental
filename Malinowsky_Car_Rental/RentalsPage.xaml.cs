94e7a766de0bdc8e68c7c369a15b6c55078a31c4
Justyna Malinowska <justyna.malinowska@microsoft.wsei.edu.pl>
2023-06-08 14:37:28 +0200

    Add `Window_Loaded`, `btnClose_Click` and `btnSave_Click`

---
+++ Malinowsky_Car_Rental/RentalsPage.xaml.cs
@@ -0,0 +1,137 @@
+﻿using Malinowsky_Car_Rental.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Malinowsky_Car_Rental
+{
+    /// <summary>
+    /// Interaction logic for RentalsPage.xaml
+    /// </summary>
+    public partial class RentalsPage : Window
+    {
+        public RentalsPage()
+        {
+            InitializeComponent();
+        }
+
+        public Wypozyczenia wypozyczenia;
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmbCustomerId.SelectedItem != null && cmbEmployeeId.SelectedItem != null && cmbCarId.SelectedItem != null
+                && datePickerRentalDate.SelectedDate != null && datePickerScheduledReturnDate.SelectedDate != null)
+            {
+                int customerId = (int)cmbCustomerId.SelectedValue;
+                int employeeId = (int)cmbEmployeeId.SelectedValue;
+                int carId = (int)cmbCarId.SelectedValue;
+                DateTime rentalDate = datePickerRentalDate.SelectedDate.Value;
+                DateTime scheduledReturnDate = datePickerScheduledReturnDate.SelectedDate.Value;
+
+                using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+                {
+                    if (wypozyczenia != null && wypozyczenia.IdWypozyczenia != 0)
+                    {
+                        int rentalId = wypozyczenia.IdWypozyczenia;
+                        Wypozyczenia update = db.Wypozyczenia.FirstOrDefault(w => w.IdWypozyczenia == rentalId);
+                        if (update == null)
+                        {
+                            MessageBox.Show("Rental not found!");
+                        }
+                        else
+                        {
+                            update.IdKlienta = customerId;
+                            update.IdPracownika = employeeId;
+                            update.IdSamochodu = carId;
+                            update.DataWypozyczenia = rentalDate;
+                            update.PlanowanaDataZwrotu = scheduledReturnDate;
+                            update.DataZwrotu = datePickerReturnDate.SelectedDate;
+                            update.OplataDodatkowa = int.Parse(txtAdditionalFee.Text); 
+                            db.SaveChanges();
+
+                            MessageBox.Show("Information has been updated!");
+                        }
+                    }
+                    else
+                    {
+                        Wypozyczenia wypozyczenia = new Wypozyczenia();
+                        wypozyczenia.IdKlienta = customerId;
+                        wypozyczenia.IdPracownika = employeeId;
+                        wypozyczenia.IdSamochodu = carId;
+                        wypozyczenia.DataWypozyczenia = rentalDate;
+                        wypozyczenia.PlanowanaDataZwrotu = scheduledReturnDate;
+                        wypozyczenia.DataZwrotu = datePickerReturnDate.SelectedDate; 
+                        wypozyczenia.OplataDodatkowa = int.Parse(txtAdditionalFee.Text); 
+
+                        db.Wypozyczenia.Add(wypozyczenia);
+                        db.SaveChanges();
+
+                        MessageBox.Show("Rental has been added!");
+                    }
+
+                    cmbCustomerId.SelectedItem = null;
+                    cmbEmployeeId.SelectedItem = null;
+                    cmbCarId.SelectedItem = null;
+                    datePickerRentalDate.SelectedDate = null;
+                    datePickerScheduledReturnDate.SelectedDate = null;
+                    datePickerReturnDate.SelectedDate = null;
+                    txtAdditionalFee.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all the required fields.");
+            }
+        }
+
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+
+                List<Samochody> typySamochodow = db.Samochody.ToList();
+                cmbCarId.ItemsSource = typySamochodow;
+                cmbCarId.DisplayMemberPath = "IdTypu";
+                cmbCarId.SelectedValuePath = "IdTypu";
+
+                List<Pracownicy> pracownicy = db.Pracownicy.ToList();
+                cmbEmployeeId.ItemsSource = pracownicy;
+                cmbEmployeeId.DisplayMemberPath = "EmployeeId";
+                cmbEmployeeId.SelectedValuePath = "EmployeeId";
+
+                List<Klienci> klienci = db.Klienci.ToList();
+                cmbCustomerId.ItemsSource = klienci;
+                cmbCustomerId.DisplayMemberPath = "CustomerId";
+                cmbCustomerId.SelectedValuePath = "CustomerId";
+            }
+
+            if (wypozyczenia != null && wypozyczenia.IdWypozyczenia != 0)
+            {
+                cmbCustomerId.SelectedValue = wypozyczenia.IdKlienta;
+                cmbEmployeeId.SelectedValue = wypozyczenia.IdPracownika;
+                cmbCarId.SelectedValue = wypozyczenia.IdSamochodu;
+                datePickerRentalDate.SelectedDate = wypozyczenia.DataWypozyczenia;
+                datePickerScheduledReturnDate.SelectedDate = wypozyczenia.PlanowanaDataZwrotu;
+                datePickerReturnDate.SelectedDate = wypozyczenia.DataZwrotu; 
+                txtAdditionalFee.Text = wypozyczenia.OplataDodatkowa.ToString(); 
+            }
+        }
+
+    }
+}
--- Malinowsky_Car_Rental/Views/RentalsList.xaml.cs
+++ Malinowsky_Car_Rental/Views/RentalsList.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Malinowsky_Car_Rental.DB;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +23,54 @@ namespace Malinowsky_Car_Rental.Views
         public RentalsList()
         {
             InitializeComponent();
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+                List<Wypozyczenia> list = db.Wypozyczenia.OrderBy(x => x.IdWypozyczenia).ToList();
+                gridRentals.ItemsSource = list;
+            }
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            RentalsPage page = new RentalsPage();
+            page.ShowDialog();
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+                List<Wypozyczenia> list = db.Wypozyczenia.ToList();
+                gridRentals.ItemsSource = list;
+            }
+        }
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Wypozyczenia wyp = (Wypozyczenia)gridRentals.SelectedItem;
+            RentalsPage page = new RentalsPage();
+            page.wypozyczenia = wyp;
+            page.ShowDialog();
+            using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+            {
+                gridRentals.ItemsSource = db.Wypozyczenia.OrderBy(x => x.IdWypozyczenia).ToList();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this record?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Wypozyczenia wyp = (Wypozyczenia)gridRentals.SelectedItem;
+                if (wyp != null)
+                {
+                    using (MalinowskyCarRentalContext db = new MalinowskyCarRentalContext())
+                    {
+                        db.Wypozyczenia.Remove(wyp);
+                        db.SaveChanges();
+                        MessageBox.Show("Record deleted successfully!");
+                        gridRentals.ItemsSource = db.Wypozyczenia.OrderBy(x => x.IdWypozyczenia).ToList();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No record selected!");
+                }
+            }
     }
 }
